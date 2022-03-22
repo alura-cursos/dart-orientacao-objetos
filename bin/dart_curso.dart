c8f966bb731129e96a7e95725f18823452e2c2f7
Ricarth Lima <rrsl@cin.ufpe.br>
2022-03-22 19:07:48 -0300

    aula02

--- bin/dart_curso.dart
+++ bin/dart_curso.dart
@@ -12,3 +12,29 @@ void main() {
     isMadura = false;
   }
 }
+
+bool funcEstaMadura(int dias) {
+  if (dias >= 30) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+mostrarMadura(String nome, int dias, {required String cor}) {
+  if (dias >= 30) {
+    print("A $nome está madura.");
+  } else {
+    print("A $nome não está madura.");
+  }
+
+  if (cor != null) {
+    print("A $nome é $cor.");
+  }
+}
+
+int funcQuantosDiasMadura(int dias) {
+  int diasParaMadura = 30;
+  int quantosDiasFaltam = diasParaMadura - dias;
+  return quantosDiasFaltam;
+}
